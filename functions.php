15d1c1a053cde72abcc02d8cd69b47c9a9806bf1
HossamOmran <hossamomran989@gmail.com>
2022-10-20 21:55:40 +0200

    add program custom post

--- functions.php
+++ functions.php
@@ -11,6 +11,27 @@ add_action('wp_enqueue_scripts', 'university_files');
 /*to add title */
 function unversity_features(){
   add_theme_support('title-tag');
-
-}
+  }
 add_action('after_setup_theme', 'unversity_features');
+function unversity_adjust_queries($query) {
+  if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+    $query->set('orderby','title');
+    $query->set('order','ASC');
+    $query->set('posts_per_page',-1);
+  }
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
+    $today=date('Ymd');
+    $query->set('meta_key','event_date');
+    $query->set('orderby','meta_value_num');
+    $query->set('order','ASC');
+    $query->set('meta_query',array(
+      array(
+        'key' => 'event_date',
+        'compare'=> '>=',
+        'value'=>  $today,
+        'type'=>'numaric'
+          )
+      ));
+  }
+}
+add_action('pre_get_posts','unversity_adjust_queries');
--- single-program.php
+++ single-program.php
@@ -16,25 +16,16 @@
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event');?> ">
-            <i class="fa fa-home" aria-hidden="true"></i> Events Home
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program');?> ">
+            <i class="fa fa-home" aria-hidden="true"></i> All programs
             </a>
              <span class="metabox__main"><?php the_title();?></span>
           </p>
         </div>
         <div class="generic-content"> <?php the_content(); ?>
         </div>
-
-
-
-
-
-
-
   </div>
-    
   <?php }
-
   get_footer();
 
 ?>
