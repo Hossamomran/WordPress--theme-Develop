52ed95077bd65e1f9c343b95a0efbfcda05e29e5
HossamOmran <hossamomran989@gmail.com>
2022-10-23 22:37:05 +0200

    professor link with programs

--- single-program.php
+++ single-program.php
@@ -1,5 +1,5 @@
 <?php
-  
+  the_ID();
   get_header();
 
   while(have_posts()) {
@@ -22,6 +22,30 @@
       <div class="generic-content"><?php the_content(); ?></div>
 
       <?php 
+          $relatedprofessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+             'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(
+               array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              )
+            )
+          ));
+
+          if ($relatedprofessors->have_posts()) {
+            echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+          while($relatedprofessors->have_posts()) {
+            $relatedprofessors->the_post(); ?>
+            <li><a href="<?php the_permalink();?> <?php the_title(); ?></a></li>
+          <?php }
+          }
+          wp_reset_postdata();
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
           'posts_per_page' => 2,
@@ -46,7 +70,7 @@
 
         if ($homepageEvents->have_posts()) {
           echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+        echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' professors</h2>';
 
         while($homepageEvents->have_posts()) {
           $homepageEvents->the_post(); ?>
